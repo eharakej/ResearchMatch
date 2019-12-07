22f536bf81188f6729644c1a472fa7cb61536d71
hskalsi <33818759+hskalsi@users.noreply.github.com>
2019-12-07 17:16:49 -0500

    Added a sample Student profile as well

--- app/src/main/java/com/example/researchmatch/FacultyProfilePage.java
+++ app/src/main/java/com/example/researchmatch/FacultyProfilePage.java
@@ -3,11 +3,13 @@ package com.example.researchmatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class FacultyProfilePage extends AppCompatActivity {
+public class FacultyProfilePage extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imageViewFProfilePic;
 
@@ -16,7 +18,7 @@ public class FacultyProfilePage extends AppCompatActivity {
     EditText editTextFFirstName, editTextFLastName, editTextFSchool, editTextFDepartment, editTextFTitle;
     EditText editTextFLocation, editTextFEducation, editTextFBio, editTextFResearchInterest;
 
-    //Not really working right now h
+    Button buttonFSaveEdits;
 
 
     @Override
@@ -31,12 +33,31 @@ public class FacultyProfilePage extends AppCompatActivity {
         findViewById(R.id.editTextFSchool);
         findViewById(R.id.editTextFDepartment);
         findViewById(R.id.editTextFTitle);
-        findViewById(R.id.editTextFLocation);
+        findViewById(R.id.editTextLocation);
         findViewById(R.id.editTextFEducation);
         findViewById(R.id.editTextFBio);
-        findViewById(R.id.editTextFResearchInterest);
+        findViewById(R.id.editTextFResearchInterests);
+
+        //findViewById(R.id.buttonSSaveEdits);
+
+        buttonFSaveEdits.setOnClickListener(this);
+
+        String autoPopulateTest = "Harpreet Kalsi";
+        char[] autoPopulateTestChar = autoPopulateTest.toCharArray();
+
+        //I have no idea what the second two things are, but if this work then we can get the stuff
+        //from the database and just convert to to a string then char array and set the editText to that
+        editTextFFirstName.setText(autoPopulateTestChar,0,0);
+
+        //Right now we need to find a way to auto populate the editTexts with the data from the database so that we can
+        //have the editTexts filled in with the user info when they get to tthis page
 
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
